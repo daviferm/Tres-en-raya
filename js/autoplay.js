dcd8128740e34a8a18896762ca676001fc6c04f2
David Fernandez <david_ferm@hotmail.com>
2018-01-07 01:35:50 +0100

    Dos Jugadores
    Actualización para dos jugadores

--- js/autoplay.js
+++ js/autoplay.js
@@ -2,12 +2,9 @@
 
 var botonX = document.getElementById("botonX");
 var botonO = document.getElementById("botonO");
+const $titulo = document.querySelector('h1');
 
 
-botonX.addEventListener("mouseover", overBotonX);
-botonX.addEventListener("mouseleave", leaveBotonX);
-botonO.addEventListener("mouseover", overBotonO);
-botonO.addEventListener("mouseleave", leaveBotonO);
 botonO.addEventListener("click", autoplay_O);
 botonX.addEventListener("click", autoplay);
 
@@ -18,23 +15,13 @@ var scroll_total = 100;
 var width_scroll = 0;
 var max_Tirada;
 var turno;
+var players;
 //turno = 0 -> X
 //turno = 1 -> O
 
-function overBotonX(){
-	document.getElementById("equex").style.transform = "scale(1.3)";
+inicioReloj();
+iniciarFecha();
 
-}
-function leaveBotonX(){
-	document.getElementById("equex").style.transform = "scale(1)";
-}
-function overBotonO(){
-	document.getElementById("boton_circulo").style.transform = "scale(1.2)";
-
-}
-function leaveBotonO(){
-	document.getElementById("boton_circulo").style.transform = "scale(1)";
-}
 var board = new Array(3);
 var Tirada = 0;
 
@@ -42,87 +29,34 @@ function autoplay_O(y, x) {
 	Tirada = 0;
 	turno = 1;
 	botonX.style.borderBottom = "0px solid #400000";
-	botonO.style.borderBottom = "4px solid #400000";
-	botonO.style.transform = "scale(1.2)";
+	botonO.style.borderBottom = "3px solid #400000";
+	botonO.style.transform = "scale(1.1)";
 	max_Tirada = 12;
-	checkCell_O();
+	if(players==1) checkCell_O();
 }
-function autoplay () {
-	inicioReloj();
-	iniciarFecha();
-	botonX.style.borderBottom = "4px solid #660066";
-	botonX.style.transform = "scale(1.2)";
+
+
+function autoplay (event) {
+	botonX.style.borderBottom = "3px solid #660066";
+	botonX.style.transform = "scale(1.1)";
 	for (i=0; i<3; i++){
 		board[i] = new Array(3);
 	}
 	max_Tirada = 12;
 	turno = 0;
-	document.getElementById("mensaje").style.display = "none";
 	ClearBoard();
-
+	MENSAJE.style.display = "none";
 	/*y=Math.round(Math.random()*2);
 	x=Math.round(Math.random()*2);*/
 }
+MENSAJE.style.display = "none";
 
-autoplay();
+// autoplay();
 // posicion 0 -> vacia
 // posicion 1 -> ocupada x
 // posicion 2 -> ocupada O
 
-/*
-switch (Tirada) {
-		case (0):
-			PaintCrossCell(y, x);
-			SelectCell(y, x);
-			botonX.style.borderBottom = "0px solid #400000";
-			botonO.style.borderBottom = "4px solid #400000";
-			break;
-		case (1):
-			PaintCircleCell(y, x);
-			botonX.style.borderBottom = "4px solid #400000";
-			botonO.style.borderBottom = "0px solid #400000";
-			break;
-		case (2):
-			PaintCrossCell(y, x);
-			botonX.style.borderBottom = "0px solid #400000";
-			botonO.style.borderBottom = "4px solid #400000";
-			break;
-		case (3):
-			PaintCircleCell(y, x);
-			botonX.style.borderBottom = "4px solid #400000";
-			botonO.style.borderBottom = "0px solid #400000";
-			break;
-		case (4):
-			PaintCrossCell(y, x);
-			botonX.style.borderBottom = "0px solid #400000";
-			botonO.style.borderBottom = "4px solid #400000";
-			break;
-		case (5):
-			PaintCircleCell(y, x);
-			botonX.style.borderBottom = "4px solid #400000";
-			botonO.style.borderBottom = "0px solid #400000";
-			break;
-		case (6):
-			PaintCrossCell(y, x);
-			botonX.style.borderBottom = "0px solid #400000";
-			botonO.style.borderBottom = "4px solid #400000";
-			break;
-		case (7):
-			PaintCircleCell(y, x);
-			botonX.style.borderBottom = "4px solid #400000";
-			botonO.style.borderBottom = "0px solid #400000";
-			break;
-		case (8):
-			PaintCrossCell(y, x);
-			botonX.style.borderBottom = "0px solid #400000";
-			botonO.style.borderBottom = "4px solid #400000";
-			break;
-		default:
-			alert("No hay mas movimientos");
-			break;
-	}
-	CellSelectec_x = x;
-	CellSelectec_y = y;
-	Tirada++;
-	console.log(Tirada);
-*/
+
+
+
+
